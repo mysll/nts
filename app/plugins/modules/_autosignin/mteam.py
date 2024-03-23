8eed9761d2f11e94398c6e575b0c25931cb50c76
mysll <liangliang.shi@gmail.com>
2024-03-23 17:33:19 +0800

    适配mteam

---
+++ app/plugins/modules/_autosignin/mteam.py
@@ -0,0 +1,65 @@
+import json
+from urllib.parse import urljoin
+import log
+from lxml import etree
+
+from app.plugins.modules._autosignin._base import _ISiteSigninHandler
+from app.utils import StringUtils, RequestUtils
+from config import Config
+
+
+class MTeam(_ISiteSigninHandler):
+    """
+    MTEAM签到
+    """
+    # 匹配的站点Url，每一个实现类都需要设置为自己的站点Url
+    site_url = "m-team.io"
+
+    @classmethod
+    def match(cls, url):
+        """
+        根据站点Url判断是否匹配当前站点签到类，大部分情况使用默认实现即可
+        :param url: 站点Url
+        :return: 是否匹配，如匹配则会调用该类的signin方法
+        """
+        return True if StringUtils.site_equal(url, cls.site_url) else False
+
+    def signin(self, site_info: dict):
+        """
+        执行签到操作
+        :param site_info: 站点信息，含有站点Url、站点Cookie、UA等信息
+        :return: 签到结果信息
+        """
+        site = site_info.get("name")
+        site_cookie = site_info.get("cookie")
+
+        if self._site_cookie == "":
+            self.error(f"签到失败，cookie 为空")
+            return False, f'【{site}】签到失败，cookie 为空'
+
+        cookie_dic = RequestUtils.cookie_parse(self._site_cookie)
+        if "token" not in cookie_dic or "user_id" not in cookie_dic:
+            self.error(f"签到失败，cookie 格式错误,token=xx;user_id=yy")
+            return False, f'【{site}】签到失败，cookie 格式错误,token=xx;user_id=yy'
+
+        self._site_cookie = cookie_dic["token"]
+        self.userid = cookie_dic["user_id"]
+
+        
+        proxy = Config().get_proxies() if site_info.get("proxy") else None
+
+        req_headers = {}
+        req_headers.update({
+                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "x-api-key": f"{self._site_cookie}"
+                })
+
+        res = RequestUtils(headers=req_headers,
+                                proxies=proxy
+                                ).post_res(url=urljoin(site_info.get("signurl"), "api/member/updateLastBrowse"))
+        
+        if res is not None and res.status_code == 200:
+            ret = json.load(res)
+            if ret.get("message") == "SUCCESS":
+                return True, f'【{site}】签到成功'
+        return False, f'【{site}】签到失败'
--- app/sites/siteuserinfo/mt_project.py
+++ app/sites/siteuserinfo/mt_project.py
@@ -2,9 +2,13 @@
 import json
 import re
 
+import log
 from app.sites.siteuserinfo._base import _ISiteUserInfo, SITE_BASE_ORDER
 from app.utils import StringUtils
+from urllib.parse import urljoin, urlsplit
 from app.utils.types import SiteSchema
+from app.utils import RequestUtils
+from config import Config
 
 
 class MTeamSiteUserInfo(_ISiteUserInfo):
@@ -15,14 +19,45 @@ class MTeamSiteUserInfo(_ISiteUserInfo):
     def match(cls, html_text):
         return 'Powered by mTorrent' in html_text
 
-    def _parse_site_page(self, html_text):
-        html_text = self._prepare_html_text(html_text)
+    def parse(self):
+        self._parse_favicon(self._index_html)
+        if not self._parse_logged_in(""):
+            return
+        self._parse_site_page("")
+        self._parse_user_base_info(self._get_page_content(urljoin(self._base_url, self._user_detail_page)))
 
-        user_detail = re.search(r"/profile/detail/(\d+)", html_text)
-        if user_detail and user_detail.group().strip():
-            self._user_detail_page = user_detail.group().strip().lstrip('/')
-            self.userid = user_detail.group(1)
-            self._torrent_seeding_page = "api/member/getUserTorrentList"
+        
+    def _parse_site_page(self, html_text):
+        self._user_detail_page = "api/member/profile"
+        self._torrent_seeding_page = "api/member/getUserTorrentList"
+        
+    def _parse_logged_in(self, html_text):
+        if self._site_cookie == "":
+            log.warn(f"【Sites】{self.site_name} cookie is null")
+            return False
+
+        cookie_dic = RequestUtils.cookie_parse(self._site_cookie)
+        if "token" not in cookie_dic or "user_id" not in cookie_dic:
+            log.warn(f"【Sites】{self.site_name} token or user_id is null")
+            return False
+
+        self._site_cookie = cookie_dic["token"]
+        self.userid = cookie_dic["user_id"]
+        return True
+
+    def _parse_user_base_info(self, html_text):
+        user = json.load(html_text) or {}
+        if user.get("message") != "SUCCESS":
+            return
+
+        data = user.get("data")
+        self.username = data.get("username")
+        member_count = data.get("memberCount")
+        self.upload = member_count.get("uploaded")
+        self.download = member_count.get("downloaded")
+        self.ratio = member_count.get("shareRate")
+        self.bonus = member_count.get("bonus")
+
 
     def _parse_user_detail_info(self, html_text):
         pass
@@ -42,3 +77,39 @@ class MTeamSiteUserInfo(_ISiteUserInfo):
         """
         # {"userid":"300094","type":"SEEDING","pageNumber":1,"pageSize":20}
         pass
+
+    def _get_page_content(self, url, params=None, headers=None):
+        #x-api-key
+        req_headers = {}
+        req_headers.update({
+                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "x-api-key": f"{self._site_cookie}"
+                })
+        proxies = Config().get_proxies() if self._proxy else None
+        if headers or self._addition_headers:
+            if headers:
+                req_headers.update(headers)
+
+            if self._addition_headers:
+                req_headers.update(self._addition_headers)
+
+        if params:
+            res = RequestUtils(session=self._session,
+                               timeout=60,
+                               proxies=proxies,
+                               headers=req_headers).post_res(url=url, data=params)
+        else:
+            res = RequestUtils(cookies=self._site_cookie,
+                               session=self._session,
+                               timeout=60,
+                               proxies=proxies,
+                               headers=req_headers).get_res(url=url)
+        
+        if res is not None and res.status_code in (200, 500, 403):
+            if "charset=utf-8" in res.text or "charset=UTF-8" in res.text:
+                res.encoding = "UTF-8"
+            else:
+                res.encoding = res.apparent_encoding
+            return res.text
+
+        return ""
